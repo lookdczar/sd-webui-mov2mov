1a45084b7071f1cb8985c26af0c2d4b1ddede207
lookdczar <443548152@qq.com>
2023-05-16 18:34:09 +0800

    Update m2m_config.py

--- scripts/m2m_config.py
+++ scripts/m2m_config.py
@@ -1,2 +1,2 @@
-mov2mov_outpath_samples = 'outputs/mov2mov-images'
-mov2mov_output_dir = 'outputs/mov2mov-videos'
+mov2mov_outpath_samples = '/data/outputs/mov2mov-images'
+mov2mov_output_dir = '/data/outputs/mov2mov-videos'
